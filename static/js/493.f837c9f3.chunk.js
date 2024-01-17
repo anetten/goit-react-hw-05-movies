2d229ca5f30ca8ab0e036b2a133ca605e9d60a5b
anetten <137095114+anetten@users.noreply.github.com>
2024-01-17 23:42:47 +0000

    Deploying to gh-pages from @ anetten/goit-react-hw-05-movies@c1a741827c6ff5b44f607ce5dd6d44b76a057b03 🚀

---
+++ static/js/493.f837c9f3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[493],{493:function(e,r,t){t.r(r);t(791);var n=t(689),i=t(87),c=t(184);r.default=function(e){var r=e.trending,t=(0,n.TH)();return r?(0,c.jsxs)("div",{children:[(0,c.jsx)("h3",{children:"Trending today"}),(0,c.jsx)("ul",{children:r.results.map((function(e){return(0,c.jsx)("li",{children:(0,c.jsx)(i.rU,{state:{from:t},to:"/movies/".concat(e.id),children:e.title})},e.id)}))})]}):(0,c.jsx)("div",{children:"Loading.."})}}}]);
+//# sourceMappingURL=493.f837c9f3.chunk.js.map
